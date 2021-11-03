bf48d8ae264ca835b7b3a0dc148a6e2daefcecd5
Bogdan Rudko <rydkoc@icloud.com>
2021-11-03 17:06:36 +0300

    Init: start new project version
    Start new project version with new technologies:
    spring web
    spring-boot
    spring data jpa
    spring security
    lombok
    thymeleaf

---
+++ src/main/java/ru/rydkoc/mapOfGrenade/MapOfGrenadeApplication.java
@@ -0,0 +1,13 @@
+package ru.rydkoc.mapOfGrenade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapOfGrenadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapOfGrenadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rydkoc/mapOfGrenade/MapOfGrenadeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rydkoc.mapOfGrenade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapOfGrenadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
