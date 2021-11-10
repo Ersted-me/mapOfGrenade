a32309cd8dad249d1f201fda4dd39b01b73bd364
Bogdan Rudko <rydkoc@icloud.com>
2021-11-10 10:41:02 +0300

    Update: add (@Repository) annotation

--- src/main/java/ru/rydkoc/mapOfGrenade/repository/RoleRepository.java
+++ src/main/java/ru/rydkoc/mapOfGrenade/repository/RoleRepository.java
@@ -1,8 +1,10 @@
 package ru.rydkoc.mapOfGrenade.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.rydkoc.mapOfGrenade.model.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
--- src/main/java/ru/rydkoc/mapOfGrenade/repository/UserRepository.java
+++ src/main/java/ru/rydkoc/mapOfGrenade/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package ru.rydkoc.mapOfGrenade.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.rydkoc.mapOfGrenade.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
 }
